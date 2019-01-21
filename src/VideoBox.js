c9454cf7ecbb0240999fa4c23b1179c9195d665b
D. Scott Boggs <scott@tams.tech>
2019-01-20 21:03:32 -0500

    eslint fix

--- src/VideoBox.js
+++ src/VideoBox.js
@@ -4,30 +4,30 @@
 // Please see LICENSE.md for terms of use and modification
 
 // A VideoBox holds a thumbnail and the title to a movie for the main page.
-import React from 'react';
-import PropTypes from 'prop-types';
-import Thumbnail from './Thumbnail';
-import { ComponentWithThumbnail, Flash } from './misc';
-import './VideoBox.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Thumbnail from './Thumbnail'
+import { ComponentWithThumbnail } from './misc'
+import './VideoBox.css'
 
 // A box to hold a video's title and its thumbnail
 class VideoBox extends ComponentWithThumbnail {
   constructor(props){
-    super(props);
-    this.clicked   = this.clicked.bind(this);
-    this.hideTitle = this.hideTitle.bind(this);
-    this.showTitle = this.showTitle.bind(this);
+    super(props)
+    this.clicked   = this.clicked.bind(this)
+    this.hideTitle = this.hideTitle.bind(this)
+    this.showTitle = this.showTitle.bind(this)
     // ComponentWithThumbnail sets state, we need to update that, not overwrite it
-    this.state.extend({ displayTitle: true });
+    this.state.extend({ displayTitle: true })
   }
-  clicked()  { this.props.OnClick(this.props.Identifier); }
-  get URL()  { return `/vid?id=${this.props.Identifier}`; }
-  get type() { return "VideoBox"; }
+  clicked()  { this.props.OnClick(this.props.Identifier) }
+  get URL()  { return `/vid?id=${this.props.Identifier}` }
+  get type() { return 'VideoBox' }
   showTitle() {
-    if( this.state.thumbnailHasLoaded ) this.setState({ displayTitle: true });
+    if( this.state.thumbnailHasLoaded ) this.setState({ displayTitle: true })
   }
   hideTitle() {
-    if( this.state.thumbnailHasLoaded ) this.setState({ displayTitle: false });
+    if( this.state.thumbnailHasLoaded ) this.setState({ displayTitle: false })
   }
   get title() {
     return (
@@ -37,11 +37,11 @@ class VideoBox extends ComponentWithThumbnail {
         <div className='video-box-centering-wrapper-inner'
              style={ {
                  opacity: this.state.displayTitle? 1: 0,
-                 transition: "opacity .3s" } }>
+                 transition: 'opacity .3s' } }>
           {this.props.Title}
         </div>
       </div>
-    );
+    )
   }
   get thumbnail() {
     if( this.state.thumbnailHasLoaded ) {
@@ -52,9 +52,9 @@ class VideoBox extends ComponentWithThumbnail {
                    }`}
                    ParentType={this.type}
                    Source={this.state.ThumbnailSource} />
-      );
+      )
     }
-    return <span className="no-thumbnail" />;
+    return <span className="no-thumbnail" />
   }
   render() {
     return (
@@ -67,7 +67,7 @@ class VideoBox extends ComponentWithThumbnail {
           {this.title}
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -78,6 +78,6 @@ VideoBox.propTypes = {
   // ThumbnailLoaded: PropTypes.func.isRequired,
   Thumbnail: PropTypes.string.isRequired,
   AuthToken: PropTypes.string.isRequired
-};
+}
 
-export default VideoBox;
+export default VideoBox
