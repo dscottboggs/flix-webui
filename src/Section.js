1eefdedfbe320d460ebd6d4c0bb1d28dbb827f58
D. Scott Boggs <scott@tams.tech>
2019-01-20 21:05:34 -0500

    ditched titles from top-level sections

--- src/Section.js
+++ src/Section.js
@@ -3,73 +3,55 @@
 
 // Please see LICENSE.md for terms of use and modification
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import VideoBox from './VideoBox';
-import Thumbnail from './Thumbnail';
-import { SectionStyle, SectionHeadStyle } from './Styles';
-import { Flash, ComponentWithThumbnail, not, Sortable } from './misc';
-import { DIRECTORY_FILE_TYPE, VIDEO_FILE_TYPE } from './FileMetadata';
-import './Section.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import VideoBox from './VideoBox'
+import Thumbnail from './Thumbnail'
+import { SectionStyle, SectionHeadStyle } from './Styles'
+import { Flash, ComponentWithThumbnail, not, Sortable } from './misc'
+import { DIRECTORY_FILE_TYPE, VIDEO_FILE_TYPE } from './FileMetadata'
+import './Section.css'
 
 export default class Section extends ComponentWithThumbnail {
   constructor(props) {
-    super(props);
-    // Flash.DEBUG(
-    //     "got Section with props\nContent: %O\nItemClicked: %O\n"
-    //   + "InitiallyExpanded: %s\nThumbnailLoaded: %O\nThumbnail: %s",
-    //   JSON.stringify(
-    //     {
-    //       ID: this.props.Content.ID,
-    //       Title: this.props.Content.Title,
-    //       Thumbnail: this.props.Content.Thumbnail,
-    //       Children: this.props.Content.Children.map(child => child.Title)
-    //     },
-    //     null,
-    //     2
-    //   ),
-    //   this.props.ItemClicked,
-    //   this.props.InitiallyExpanded? "true": "false",
-    //   this.props.ThumbnailLoaded,
-    //   this.props.Thumbnail
-    // );
-    this.sort = this.sort.bind(this);
-    this.make = this.make.bind(this);
-    this.makeVideoComponent = this.makeVideoComponent.bind(this);
-    this.makeComponent = this.makeComponent.bind(this);
-    this.handleClickedHeader = this.handleClickedHeader.bind(this);
-    this.handleMouseOverHeader = this.handleMouseOverHeader.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    this.state.extend({ expanded: this.props.InitiallyExpanded });
+    super(props)
+    this.sort = this.sort.bind(this)
+    this.make = this.make.bind(this)
+    this.makeVideoComponent = this.makeVideoComponent.bind(this)
+    this.makeComponent = this.makeComponent.bind(this)
+    this.handleClickedHeader = this.handleClickedHeader.bind(this)
+    this.handleMouseOverHeader = this.handleMouseOverHeader.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    this.state.extend({ expanded: this.props.InitiallyExpanded })
   }
   componentDidMount() {
-    ComponentWithThumbnail.prototype.componentDidMount.call(this);
+    ComponentWithThumbnail.prototype.componentDidMount.call(this)
     this.sort(this.props.Content.Children)
         .then( sorted => this.make(sorted))
         .then( made => this.setState({filteredAndSorted: made }) )
         .catch( err => {
-            Flash.ERROR(err);
+            Flash.ERROR(err)
             // eslint-disable-next-line promise/no-nesting
             this.make(this.props.Content.Children)
               .then(made => this.setState( {filteredAndSorted: made.values} ) )
               .catch(err => Flash.CRITICAL(
                 "failed to display even unsorted media! Please report this, it's "+
-                "definitely a bug. Error message: " + err));
-          });
+                'definitely a bug. Error message: ' + err))
+          })
   }
   get type() {
-    return "Section";
+    return 'Section'
   }
   get style() {
-    return SectionStyle(this);
+    return SectionStyle(this)
   }
   get headStyle() {
-    return SectionHeadStyle(this);
+    return SectionHeadStyle(this)
   }
-  get numberOfChildren() { return this.props.Content.Children.keys.length; }
+  get numberOfChildren() { return this.props.Content.Children.keys.length }
   // Top-level directories are numerically assigned, others according
   // to their hash
-  get isNotTopLevel() { return isNaN(Number(this.props.Content.ID)); }
+  get isNotTopLevel() { return isNaN(Number(this.props.Content.ID)) }
   get content() {
     if (this.state.filteredAndSorted && this.state.expanded ){
       return (
@@ -77,43 +59,43 @@ export default class Section extends ComponentWithThumbnail {
              id={`section-content-${this.props.Content.Title}`}>
           {this.state.filteredAndSorted}
         </div>
-      );
+      )
     } else {
-      return <span className='section-content hidden'/>;
+      return <span className='section-content hidden'/>
     }
   }
   get HeaderContent() {
-    if( this.state.thumbnailHasLoaded ){
-      return (
-        <div className={`section-header-content${this.isNotTopLevel?'':" top-level"}`}
-             id={this.props.Content.Title}
-             onMouseOver={ this.handleMouseOverHeader }>
-          <Thumbnail Identifier={this.props.Content.ID}
-                     ParentType={this.type}
-                     Source={this.state.ThumbnailSource}/>
-          {this.props.Content.Title}
-        </div>
-      );
-    } else {
-      return (
-        <div className='section-header-content'
-             onMouseOver={ this.handleMouseOverHeader }>
-          {this.props.Content.Title}
-        </div>
-      );
+    if (not(this.isNotTopLevel)) {
+      return null
     }
+    if( this.state.thumbnailHasLoaded ) return (
+      <div className={`section-header-content${this.isNotTopLevel?'':' top-level'}`}
+           id={this.props.Content.Title}
+           onMouseOver={ this.handleMouseOverHeader }>
+        <Thumbnail Identifier={this.props.Content.ID}
+                   ParentType={this.type}
+                   Source={this.state.ThumbnailSource}/>
+        {this.props.Content.Title}
+      </div>
+    )
+    return (
+      <div className='section-header-content'
+           onMouseOver={ this.handleMouseOverHeader }>
+        {this.props.Content.Title}
+      </div>
+    )
 
   }
-  get headerPadding() { return "1.2em"; }
+  get headerPadding() { return '1.2em' }
   handleMouseOverHeader() {
     if( this.isNotTopLevel ) this.setState(
       {hasMouseFocus: true},
       () => {
         setTimeout(() => {
-          if( this.state.hasMouseFocus ) this.setState({expanded: true});
-        }, 200);
+          if( this.state.hasMouseFocus ) this.setState({expanded: true})
+        }, 200)
       }
-    );
+    )
   }
   handleMouseLeave() {
     if( this.isNotTopLevel ) this.setState(
@@ -121,15 +103,15 @@ export default class Section extends ComponentWithThumbnail {
       () => {
         setTimeout(
           () => {
-            if( not(this.state.hasMouseFocus) ) this.setState({expanded: false});
+            if( not(this.state.hasMouseFocus) ) this.setState({expanded: false})
           },
           750
-        );
+        )
       }
-    );
+    )
   }
   handleClickedHeader() {
-    if( this.isNotTopLevel ) this.setState({expanded: !this.state.expanded});
+    if( this.isNotTopLevel ) this.setState({expanded: !this.state.expanded})
   }
   makeComponent(content){
     if( content.Type === DIRECTORY_FILE_TYPE ) return (
@@ -140,10 +122,10 @@ export default class Section extends ComponentWithThumbnail {
                ThumbnailLoaded={this.props.ThumbnailLoaded}
                Thumbnail={content.Thumbnail}
                AuthToken={this.props.AuthToken}/>
-    );
+    )
     if( content.Type === VIDEO_FILE_TYPE ) return (
       <VideoBox {...this.makeVideoComponent(content)} />
-    );
+    )
     // Flash.WARNING(
     //   `got unknown typed content in section ${this.props.Title}
     //   content: ${JSON.stringify(content)}
@@ -162,40 +144,40 @@ export default class Section extends ComponentWithThumbnail {
       ThumbnailLoaded: () => this.props.ThumbnailLoaded(video.ID),
       Thumbnail: video.ID,
       AuthToken: this.props.AuthToken
-    };
+    }
   }
   // Sort the components in this section. Videos come first, then Directories
   sortFunction(elA, elB) {
     if( elA.value.Type === DIRECTORY_FILE_TYPE ) {
       if( elB.value.Type === DIRECTORY_FILE_TYPE ) {
         // fall back on sorting by keys in alphabetical order
-        return (elA.key > elB.key)? Sortable.swap : Sortable.noSwap;
+        return (elA.key > elB.key)? Sortable.swap : Sortable.noSwap
       }
       if( elB.value.Type !== VIDEO_FILE_TYPE ) throw new TypeError(
         `got unknown filetype at key ${elB.key}`
-      );
+      )
       // elB is a Video, it should go to the top.
-      return Sortable.swap;
+      return Sortable.swap
     }
     if( elB.value.Type === VIDEO_FILE_TYPE ) {
       // fall back on sorting by keys in alphabetical order
-      return (elA.key > elB.key)? Sortable.swap: Sortable.noSwap;
+      return (elA.key > elB.key)? Sortable.swap: Sortable.noSwap
     }
     if( elB.value.Type !== DIRECTORY_FILE_TYPE ) throw new TypeError(
       `got unknown filetype at key ${elB.key}`
-    );
+    )
     // elA is a Video, and elB is a Directory
-    return Sortable.noSwap;
+    return Sortable.noSwap
   }
   sort(children) {
     return new Promise((resolve, reject) => {
       new Sortable(children).sortWith(this.sortFunction)
                             .then(resolve)
-                            .catch(reject);
-    });
+                            .catch(reject)
+    })
   }
   async make(children) {
-    return children.map(this.makeComponent);
+    return children.map(this.makeComponent)
   }
   render() {
     return (
@@ -211,7 +193,7 @@ export default class Section extends ComponentWithThumbnail {
         </div>
         {this.content}
       </div>
-    );
+    )
   }
 }
 
@@ -222,4 +204,4 @@ Section.propTypes = {
   InitiallyExpanded: PropTypes.bool.isRequired,
   Thumbnail: PropTypes.string,
   AuthToken: PropTypes.string.isRequired
-};
+}
